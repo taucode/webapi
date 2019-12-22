539bd199ca388e116e425f25acf3919063ff67de
Andrey Kovalenko <ak@taucode.com>
2019-12-22 19:13:38 +0200

    Started reorganizing.

--- tests/TauCode.WebApi.Client.Tests/Factory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using TauCode.WebApi.Client.Tests.App;
-
-namespace TauCode.WebApi.Client.Tests
-{
-    public class Factory : WebApplicationFactory<Startup>
-    {
-    }
-}
